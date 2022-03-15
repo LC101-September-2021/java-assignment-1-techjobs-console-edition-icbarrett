36719cf3e734a4728c57073e1bf41d94976a9196
Ingrid Barrett <barrett92@yahoo.com>
2022-03-15 19:38:04 -0400

    1st commit: no result message working.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,9 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        }
         System.out.println("printJobs is not implemented yet");
     }
 }
